73bcac9ea8f795491cad996a11a369721358c60b
Sina Dashti <sina.v.dashti@gmail.com>
2020-06-13 20:36:41 +0200

    Format run.py based on black

--- run.py
+++ run.py
@@ -5,36 +5,39 @@ import string
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 urls = []
+
 
 def url_validator(url):
     result = urlparse(url)
     return all([result.scheme, result.netloc, result.path])
 
-def randomword():
-   letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
-   return ''.join(random.choice(letters) for i in range(6))
+
+def random_word():
+    letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return "".join(random.choice(letters) for i in range(6))
+
 
 def short_out(random_text):
-    return f'https://Koojilink/{random_text}'
+    return f"https://koojelink/{random_text}"
 
 
-@app.route('/', methods=['GET', 'POST'])
-def test_post():
-
-    if request.method == 'POST':
-        input_url = {'long_url':request.json['long_url']}
-        if url_validator(input_url.get('long_url')):
-            output_url = {'short_url':short_out(randomword())}
+@app.route("/", methods=["GET", "POST"])
+def handle_post_request():
+    if request.method == "POST":
+        input_url = {"long_url": request.json["long_url"]}
+        if url_validator(input_url.get("long_url")):
+            output_url = {"short_url": short_out(randomword())}
             urls.append([input_url, output_url])
             # return jsonify({'urls':urls})
-            return output_url.get('short_url')
+            return output_url.get("short_url")
         else:
-            return 'error'
+            return jsonify({"error": "wrong key:value input"})
     else:
-        return jsonify({'message':'It works !!!'})
+        return jsonify({"message": "Welcome to koojelink!"})
 
-if __name__ == '__main__':
-    app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
