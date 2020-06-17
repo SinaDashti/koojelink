16656f083d43c4b129830c2b73d9882a0c8d4101
Sina Dashti <sina.v.dashti@gmail.com>
2020-06-18 00:43:33 +0200

    redirect to origin based on the short link.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 import re
 import random
 import string
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-# urls = []
+urls = []
 
 
 def url_validator(url):
@@ -33,19 +33,34 @@ def short_out(random_text):
     return f"https://koojelink/{random_text}"
 
 
+# without database
+def search_for_url(all_urls, random_text):
+    for url in all_urls:
+        if url[1]["short_url"].split("k/")[1] == random_text:
+            return url[0]["long_url"]
+
+    # just for test, it should change !
+    return "https://www.manutd.com"
+
+
 @app.route("/", methods=["GET", "POST"])
 def handle_post_request():
     if request.method == "POST":
         input_url = {"long_url": request.json["long_url"]}
         if url_validator(input_url.get("long_url")):
             output_url = {"short_url": short_out(random_word())}
-            # urls.append([input_url, output_url])
+            urls.append([input_url, output_url])
             # return jsonify({'urls':urls})
             return output_url.get("short_url")
         else:
             return jsonify({"error": "wrong key:value input"})
     else:
         return jsonify({"message": "Welcome to koojelink!"})
+
+
+@app.route("/koojelink/<string:random_text>")
+def redirect_to_origin(random_text):
+    return redirect(search_for_url(urls, random_text))
 
 
 if __name__ == "__main__":
